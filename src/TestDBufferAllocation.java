6a84c9bf4cfb1a19a22f3b1583115aaac11eaa2d
vilas <vilas@10.0.0.2>
2015-03-29 10:00:56 -0700

    test to find available direct byte buffers

---
+++ src/TestDBufferAllocation.java
@@ -0,0 +1,20 @@
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class TestDBufferAllocation {
+	public static void main(String[] args) {
+//		int i = 0;
+		List<ByteBuffer> list = new ArrayList<>();
+		try {
+			while(true) {
+				list.add(ByteBuffer.allocateDirect(2048));
+//				i++;
+			}
+		} catch(Throwable t) {
+			System.out.println("allocated: " + list.size());
+			t.printStackTrace();
+		}
+	}
+}
